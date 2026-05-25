57d4586ab72051f73e8b0d4a06d5d1c666d59689
Fred Morrison <frederick.morrison@outlook.com>
2026-05-25 10:56:19 -0600

    include elapsed time in the output

--- Install-npm-packages.ps1
+++ Install-npm-packages.ps1
@@ -3,30 +3,49 @@
              with care taken to verify the npm cache after each install, a lesson learned the hard way.
     Author: Fred Morrison  
     Location: C:\Users\frede\OneDrive\PowerShell\Install-npm-packages.ps1
-#>
-$previousVerbosePreference = $VerbosePreference
-if ($VerbosePreference -ne 'Continue') { $VerbosePreference = 'Continue' }
 
+    MODIFY THE FOLLOWING COMMA-SEPARATED LIST WITH PACKAGE NAMES THAT NEED TO BE UPDATED TO THE LATEST VERSION
+#>
+param(
+    [string[]] $cmds = @('webpack')
+)
+$previousVerbosePreference = $VerbosePreference
+$VerbosePreference = 'Continue'
 # set the date/time display format to be similar to ISO 8601
 Set-Variable -Name 'dateFormat' -Value 'yyyy-MM-dd HH:mm:ss' -ErrorAction SilentlyContinue
-Get-Date -Format $dateFormat
+$startDateTime = Get-Date 
+$displayDateTime = $startDateTime.ToString($dateFormat)
+Write-Verbose -Message "Started at $displayDateTime"
 
 Get-Location -Verbose
-# MODIFY THE FOLLOWING COMMA-SEPARATED LIST WITH PACKAGE NAMES THAT NEED TO BE UPDATED TO THE LATEST VERSION
-[string[]] $cmds = @('webpack')
+
 [string] $cmd = ''
 $cmds | ForEach-Object  -Process {    
   $cmd = 'npm install --location=global {0}@latest' -f $_
   Write-Verbose -Message $cmd
-  Invoke-Command -ScriptBlock ([ScriptBlock]::Create($cmd)) -Verbose
+  Invoke-Command -ScriptBlock ([ScriptBlock]::Create($cmd))
   
   Get-Date -Format $dateFormat
   
   $cmd = 'npm cache verify'
   Write-Verbose -Message $cmd
-  Invoke-Command -ScriptBlock ([ScriptBlock]::Create($cmd)) -Verbose
+  Invoke-Command -ScriptBlock ([ScriptBlock]::Create($cmd))
   
   Get-Date -Format $dateFormat
 }
 
+$endDateTime = Get-Date
+$displayDateTime = $endDateTime.ToString($dateFormat)
+Write-Verbose -Message "Ended at $displayDateTime"
+
+[TimeSpan] $elapsed = $endDateTime - $startDateTime
+$elapsedFormatted = '{0:0000}-{1:00}:{2:00}:{3:00}' -f `
+    $elapsed.Days,
+    $elapsed.Hours,
+    $elapsed.Minutes,
+    $elapsed.Seconds
+	
+Write-Verbose -Message "Elapsed time (days-hh:mm:ss) $elapsedFormatted"
+
+# restore user's verbose preference
 $VerbosePreference = $previousVerbosePreference
